5dc996ed8e24ce45635438cf975911ae72b445ce
Krischyy <topaz_95@abv.bg>
2016-06-17 15:45:48 +0300

    Add files via upload

---
+++ CATFD.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+main (argc,argv)
+ int argc;
+ char *argv[];
+{
+  int flg = 0;
+
+  while (argc-- > 1)
+   ct (argv[++flg]);
+
+  if ( flg == 0 )
+   ct ( (char *)0 );  /*   ct (0);  */
+}
+
+ct(s)
+char *s;
+{
+  char buffer[BUFSIZ];
+  int length; int fd =0;
+
+  if (s)
+   if ( (fd=open(s,0)) == -1 ) {
+        printf("\ncat: can't open %s\n",s);
+        exit(1);
+   }
+  while (length=read(fd,buffer,BUFSIZ) )
+   write (1,buffer,length);
+ }
